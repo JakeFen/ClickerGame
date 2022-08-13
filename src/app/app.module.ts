768a2c179518a3cd99a5f445ddb7224b4e1d948b
Jacob <jacobfenwick111@gmail.com>
2022-08-13 15:07:37 -0400

    set up guard and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Guards
+import { GameGuard } from './core/guards/app.gameGuard';
+
+// Features
+import { MenuComponent } from './features/menu//menu.component';
+import { GameComponent } from './features/game/game.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'menu',
+  },
+  {
+    path: 'menu',
+    pathMatch: 'full',
+    component: MenuComponent,
+  },
+  {
+    path: 'game',
+    pathMatch: 'full',
+    canActivate: [GameGuard],
+    component: GameComponent,
+  },
+];
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent, GameComponent],
   imports: [
+    RouterModule.forRoot(routes),
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
